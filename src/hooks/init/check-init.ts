5396f33e1982f35519ed65141aeb3f7e1c666702
Satheesh kumar <mail@satheesh.dev>
2021-08-31 14:56:54 +0530

    fixes release failure

--- src/hooks/init/check-init.ts
+++ src/hooks/init/check-init.ts
@@ -4,6 +4,9 @@ import * as chalk from 'chalk'
 import * as fs from 'fs'
 
 const hook: Hook<'init'> = async function (opts) {
+  if (opts.id == "readme") {
+    return
+  }
   if (!fs.existsSync(`${opts.config.configDir}/hckre.ini`) && opts.id !== 'init') {
     this.error(`CLI not initialized. Use ${chalk.cyan(chalk.bold('hckre init'))} to initialize.`)
   }
